d982b456ee43877cbf948aedb40c261eda4659a5
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-13 01:52:32 +0530

    Add files via upload

---
+++ Print_Type.py
@@ -0,0 +1,49 @@
+"""
+Day: Thursday
+Date: 13th Jan 2023
+Time: 1 AM
+Topic: Print Types
+Author: Areeb Ahmed
+"""
+
+# print('hello world')
+# print("hello world")
+# print('hello wo"rl\'d')
+# print("hello pyt'hon")
+
+# print('''hello 
+# python''')
+
+# print('''hello
+#          world''')
+
+# print('''hello
+
+#          python''')
+
+# print('''Areeb Ahmed''')
+# print("""Areeb Ahmed""")
+
+# print('hello py\'thon')
+# print("hello py'thon")
+# print('''hello py'thon''')
+# print("""hello py'thon""")
+
+# print('print(hello Areeb)')
+# print("print(hello Areeb)")
+# print('''print(hello Areeb)''')
+# print("""print(hello Areeb)""")
+
+
+# print('print(hello Ar\'eeb)')
+# print("print(hello Ar'eeb)")
+# print('''print(hello Ar'eeb)''')
+# print("""print(hello Ar'eeb)""")
+
+# print('Areeb' 'Ahmed')
+# print('Areeb' , 'Ahmed')
+
+# print("Python " * 5)
+# print(5 * "Python ")
+print('4 '* 4)
+print('hello python3 '* 2)
